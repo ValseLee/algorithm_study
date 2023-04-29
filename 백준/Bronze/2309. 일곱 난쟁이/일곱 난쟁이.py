bd6559f91c41c5cb73762ebab22cf2d153adf8fb
승준 <82270058+ValseLee@users.noreply.github.com>
2023-04-29 15:16:32 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,12 +1,11 @@
-arr = []
-for i in range(9):
-    arr.append(int(input()))
-    
+arr = [int(input()) for _ in range(9)]
+
 for i in arr:
     for j in arr:
         if sum(arr) - i - j == 100 and i != j:
             arr.remove(i)
             arr.remove(j)
+
 arr.sort()            
 
 for i in arr:
