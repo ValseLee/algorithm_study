9b50473e2779c0803c8d8592e8b967bd4fa90f3b
승준(Celan) <82270058+ValseLee@users.noreply.github.com>
2023-05-10 18:31:05 +0900

    [level 1] Title: K번째수, Time: 0.14 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.swift
+++ 프로그래머스/lv1/42748. K번째수/K번째수.swift
@@ -2,11 +2,13 @@ import Foundation
 
 func solution(_ array: [Int], _ commands: [[Int]]) -> [Int] {
     var answer = [Int]()
-
     for i in commands {
         let start = i[0] - 1
         let end = i[1] - 1
-        answer.append(array[start ... end].sorted(by: <)[i[2] - 1])
+        answer.append(
+            array[start ... end]
+            .sorted(by: <)[i[2] - 1]
+        )
     }
     return answer
 }
