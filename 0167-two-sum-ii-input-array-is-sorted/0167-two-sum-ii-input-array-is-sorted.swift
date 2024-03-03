96c10646b385efe55627f571b1777343df018d82
Lee Seungjun <82270058+ValseLee@users.noreply.github.com>
2024-03-03 15:18:06 +0900

    Time: 28 ms (65.18%), Space: 16.6 MB (19.17%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.swift
@@ -0,0 +1,17 @@
+class Solution {
+  func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+    var ans = [Int]()
+    var left = 0
+    var right = numbers.count - 1
+
+    while left < right {
+      let sum = numbers[left] + numbers[right]
+
+      if sum == target { break }
+      else if sum < target { left += 1 }
+      else if sum > target { right -= 1 }
+    }
+
+    return [left + 1, right + 1]
+  }
+}
