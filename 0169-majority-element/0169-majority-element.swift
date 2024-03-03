fe5b1c948546443b1262aea43f729fbbcc86c3f0
Lee Seungjun <82270058+ValseLee@users.noreply.github.com>
2024-03-03 15:26:44 +0900

    Time: 54 ms (42.31%), Space: 17.3 MB (37.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.swift
+++ 0169-majority-element/0169-majority-element.swift
@@ -1,6 +1,25 @@
 class Solution {
   // 5할 최빈값
+  // func majorityElement(_ nums: [Int]) -> Int {
+  //   nums.sorted()[nums.count / 2]
+  // }
+
   func majorityElement(_ nums: [Int]) -> Int {
-    nums.sorted()[nums.count / 2]
+    var dict = [Int: Int]()
+
+    nums.forEach {
+      if let val = dict[$0] {
+        dict[$0] = val + 1
+      } else {
+        dict[$0] = 1
+      }
+    }
+
+    let mid = nums.count / 2
+    for key in dict.keys {
+      if let val = dict[key], val > mid { return key }
+    }
+
+    return 0
   }
 }
