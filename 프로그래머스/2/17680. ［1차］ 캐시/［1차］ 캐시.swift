c3a8e4c537bcc745133f89ce3de564299dee5cd9
Lee Seungjun | Celan <82270058+ValseLee@users.noreply.github.com>
2024-03-12 16:25:15 +0900

    [level 2] Title: [1차] 캐시, Time: 5.57 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
@@ -3,31 +3,22 @@ func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
     
     var res = 0
     var queue = Array(repeating: "", count: cacheSize)
-
+    
     for i in 0..<cities.count {
         let target = cities[i].lowercased()
-        
-        if queue.count < cacheSize {
-            if let idx = queue.firstIndex(of: target) {
-                queue.remove(at: idx)
-                queue.append(target)
-                res += 1
-            } else {
-                queue.append(target)
-                res += 5
-            }
+        print(queue, target)
+        if let idx = queue.firstIndex(of: target) {
+            queue.append(queue.remove(at: idx))
+            res += 1
+        } else if i < cacheSize {
+            queue[i] = target
+            res += 5
         } else {
-            if let idx = queue.firstIndex(of: target) {
-                queue.remove(at: idx)
-                queue.append(target)
-                res += 1
-            } else {
-                res += 5
-                queue.removeFirst()
-                queue.append(target)
-            }
+            queue.removeFirst()
+            queue.append(target)
+            res += 5
         }
     }
-    
+    print("RES", queue)
     return res
 }
