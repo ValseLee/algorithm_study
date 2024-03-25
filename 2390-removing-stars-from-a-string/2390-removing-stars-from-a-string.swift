7c9b972eb976f669d6a6c08f6fbdbfcaee9b348e
Lee Seungjun | Celan <82270058+ValseLee@users.noreply.github.com>
2024-03-25 16:23:32 +0900

    Time: 162 ms (97.58%), Space: 19.8 MB (23.03%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.swift
@@ -0,0 +1,14 @@
+class Solution {
+  func removeStars(_ s: String) -> String {
+    var stack = [Character]()
+    for char in s {
+      if char == "*" {
+        stack.removeLast()
+      } else {
+        stack.append(char)
+      }
+    }
+
+    return String(stack)
+  }
+}
